30e20750905b52922389177c6b2eb92a1c81a562
lemani <lemani@duck.com>
2026-06-28 13:51:38 -0300

    feat: add ani-tupi formula v2.1.0

---
+++ Formula/ani-tupi.rb
@@ -0,0 +1,24 @@
+class AniTupi < Formula
+  desc "Assista anime e leia mangá direto do terminal sem anúncios"
+  homepage "https://github.com/levyvix/ani-tupi"
+  url "https://github.com/levyvix/ani-tupi/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "3e0ad0e7a2412619b379738b2ab3e7edf3c03a04240e8b1f59c9d954ba355c0b"
+  license "GPL-3.0-only"
+  head "https://github.com/levyvix/ani-tupi.git", branch: "master"
+
+  depends_on "python@3.12"
+  depends_on "mpv"
+
+  def install
+    venv = libexec/"venv"
+    system Formula["python@3.12"].opt_bin/"python3.12", "-m", "venv", venv
+    system venv/"bin/pip", "install", "--upgrade", "pip"
+    system venv/"bin/pip", "install", "."
+    bin.install_symlink venv/"bin/ani-tupi"
+    bin.install_symlink venv/"bin/manga-tupi"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/ani-tupi --help")
+  end
+end
